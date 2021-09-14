3f8f3bf97681a710b1f3a81e0f5654525bc7228b
Your Name <shelbyfayemo@gmail.com>
2021-09-13 22:39:42 -0400

    got button working, removed map, added places api but have error

--- Data/script.js
+++ Data/script.js
@@ -1,43 +1,16 @@
-// mapbox token 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGF0c2hlbGJ5ZmF5ZSIsImEiOiJja3RnM3dvMWowZWphMnBvbGxmaG44Z2ZmIn0.yzzlQyZ8mHiQBpckxLUcOQ';
-// google apikey
-googleApikey = 'AIzaSyCQwhxC1XQdwl0B2Aqpb5S6fQCpXOvC0W8';
+var googleApikey = "AIzaSyCQwhxC1XQdwl0B2Aqpb5S6fQCpXOvC0W8";
+var input = document.querySelector(".input");
 
-//search button + input box
-var searchInput = document.querySelector('#searchInput')
-var searchButton = document.querySelector('#searchButton')
-
-// button and fetch response
-searchButton.addEventListener('click', function(){
-fetch('')
-
-    .then( response => response.json())
-    .then( data =>console.log(data))
-    })
-
-//finds current location, brings pop up box to ask allow or deny location
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-    enableHighAccuracy: true})
-    
-// uses lat+lon to bring up location
-    function successLocation(position){
-console.log(position)
-setupMap([position.coords.longitude, position.coords.latitude])
-    }
-// sets default location to Charlotte
-    function errorLocation(){
-setupMap([-80.84, 35.2])
-    }
-//map setup 
-function setupMap(center){
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
-        zoom:9
-        })
-}
-
-
-   
-  
+$("#searchButton").click(function () {
+  console.log("button");
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    fetch(
+      `https://maps.googleapis.com/maps/api/place/details/json?${input.value}&key=${googleApikey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+});
